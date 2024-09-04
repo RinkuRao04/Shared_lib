b1ddb34e974613426db9351ca253b4f6b959c75d
RinkuRao04 <rinku@gkmit.co>
2024-09-05 01:19:52 +0530

    Update githubcheckout.groovy

--- vars/githubcheckout.groovy
+++ vars/githubcheckout.groovy
@@ -1,4 +1,4 @@
 def call(Map config) {
 // checkout scmGit(branches: [[name: config.branch]], extensions: [], userRemoteConfigs: [[config.url]])
-   checkout scmGit(branches: [[name: config.url]], extensions: [], userRemoteConfigs: [[url: config.url]])
+   checkout scmGit(branches: [[name: "config.url"]], extensions: [], userRemoteConfigs: [[url: "config.url"]])
 }
