9413503198a9bfefb3a9ea27c6698128f2e35111
RinkuRao04 <rinku@gkmit.co>
2024-09-05 11:33:04 +0530

    Update bucket.groovy

--- vars/bucket.groovy
+++ vars/bucket.groovy
@@ -1,5 +1,5 @@
 def call (Map config){
     withAWS(credentials: "${config.aws_cred}" , region: "${config.region}") {
-    sh """ aws s3 sync ${config.buildPath}/ s3://${config.bucket}/ --region ${config.region} --metadata Content-Type=text/html """
+        sh """ aws s3 sync ${config.buildPath}/ s3://${config.bucket}/ --region ${config.region} --metadata Content-Type=text/html """
     }
 }
